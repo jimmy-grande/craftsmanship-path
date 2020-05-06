43e8a48c9f95a3e80c45c3c086fb043101c74407
Jimmy Grande <jimmy.grande@outlook.com>
2020-05-06 19:26:23 +0200

    fix srp typescript example

--- 1-CLEAN_CODE/SOLID/SingleResponsability/TS/dont/userService.ts
+++ 1-CLEAN_CODE/SOLID/SingleResponsability/TS/dont/userService.ts
@@ -1,57 +1,21 @@
-// DON'T
-namespace Solid.SRP.Bad {
-  class Rectangle {
-    public width: number;
-    public height: number;
-    constructor(width: number, height: number) {
-      this.width = width;
-      this.height = height;
-    }
+export class UserService {
+  constructor() {
+    
+  }
+  private validateEmail(email: string): boolean {
+    return email.includes('@')
   }
 
-  class Circle {
-    public radius: number;
-    constructor(radius: number) {
-      this.radius = radius;
-    }
+  private sendEmail(content: string): void {
+    // code to send your email
   }
-
-  function calculateAreasOfMultipleShapes(shapes: Array<Rectangle | Circle>) {
-    return shapes.reduce((calculatedArea, shape) => {
-      if (shape instanceof Rectangle) {
-        return calculatedArea + shape.width * shape.height;
-      }
-      if (shape instanceof Circle) {
-        return calculatedArea + shape.radius * Math.PI;
-      }
-    }, 0);
-  }
-}
-
-// DO
-namespace Solid.SRP.Good {
-  interface Shape {
-    getArea(): number;
-  }
-  class Rectangle implements Shape {
-    public width: number;
-    public height: number;
-    constructor(width: number, height: number) {
-      this.width = width;
-      this.height = height;
+  create(userData) {
+    if (!this.validateEmail(userData.email)) {
+      throw new Error("User email is not valid");
     }
-    public getArea() {
-      return this.width * this.height;
-    }
-  }
-
-  class Circle implements Shape {
-    public radius: number;
-    constructor(radius: number) {
-      this.radius = radius;
-    }
-    public getArea() {
-      return this.radius * Math.PI;
-    }
+    // code to create user
+    this.sendEmail(
+      `Hello ${userData.firstName} ${userData.lastName}. Your account has been created!`
+    );
   }
 }
